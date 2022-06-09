1dc6c16ff321c0ef26265ad6d72d5351b5b90dbd
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-06-09 21:04:55 +0300

    commit tree f98a2c68e9fca518ef923e7e365711e479fce056

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number) {
+  if (number <= 400) {
+    return 'This one is on me!';
+  } else if (number > 2000 && number < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (number > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName) {
+  if (cityName.toUpperCase() == 'NYC') {
+    return "Ok, sounds good.";
+  } else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipDescription) {
+  if (tipDescription.toLowerCase() == "generous") {
+    return "Thank you so much.";
+  } else if (tipDescription.toLowerCase() == "not as generous") {
+    return "Thank you.";
+  }else{
+    return "Bye.";
+  }
 }
